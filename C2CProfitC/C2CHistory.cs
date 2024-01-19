027831696f26e54c90f7e835c0c519a820f87441
kalcala <the-punisher_01@hotmail.com>
2024-01-19 02:02:20 -0400

    migracion from azure devops

---
+++ C2CProfitC/C2CHistory.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2CProfitC
+{
+    public class C2CHistory
+    {
+        public string orderNumber;
+        public string tradeType;
+        public string asset;
+        public string fiat;
+        public string fiatSymbol;
+        public double amount;
+        public double totalPrice;
+        public double unitPrice;
+		public string orderStatus;
+        // private DateTime createTime ;
+        public double commission;
+        public string counterPartNickName;
+        public string advertisementRole; 
+
+
+        public C2CHistory(string orderNumber, string tradeType, string asset, string fiat, string fiatSymbol, double amount, double totalPrice, double unitPrice, string orderStatus, double commission, string counterPartNickName, string advertisementRole)
+        {
+            this.orderNumber = orderNumber;
+            this.tradeType = tradeType;
+            this.asset = asset;
+            this.fiat = fiat;
+            this.fiatSymbol = fiatSymbol;
+            this.amount = amount;
+            this.totalPrice = totalPrice;
+            this.unitPrice = unitPrice;
+            this.orderStatus = orderStatus;
+            //this.createTime = createTime;
+            this.commission = commission;
+            this.counterPartNickName = counterPartNickName;
+            this.advertisementRole = advertisementRole;
+        }
+
+        public string OrderNumber
+        {
+            get { return orderNumber; }
+            set { orderNumber = value; }
+        }
+
+        public string TradeType
+        {
+            get { return tradeType; }
+            set { tradeType = value; }
+        }
+
+        public string Asset
+        {
+            get { return asset; }
+            set { asset = value; }
+        }
+
+        public string Fiat
+        {
+            get { return fiat; }
+            set { fiat = value; }
+        }
+
+        public string FiatSymbol
+        {
+            get { return fiatSymbol; }
+            set { fiatSymbol = value; }
+        }
+
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public double TotalPrice
+        {
+            get { return totalPrice; }
+            set { totalPrice = value; }
+        }
+
+        public double UnitPrice
+        {
+            get { return unitPrice; }
+            set { unitPrice = value; }
+        }
+
+        public string OrderStatus
+        {
+            get { return orderStatus; }
+            set { orderStatus = value; }
+        }
+
+        public double Commission
+        {
+            get { return commission; }
+            set { commission = value; }
+        }
+
+        public string CounterPartNickName
+        {
+            get { return counterPartNickName; }
+            set { counterPartNickName = value; }
+        }
+
+        public string AdvertisementRole
+        {
+            get { return advertisementRole; }
+            set { advertisementRole = value; }
+        }
+
+
+    }
+
+
+
+}
---
+++ C2CProfitC/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace C2CProfitC
+{
+
+    using System;
+    using System.Threading.Tasks;
+    using Binance.Spot;
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Binance.Common;
+    using Binance.Spot;
+    using Binance.Spot.Models;
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using System.Linq;
+
+    //dotnet add package Binance.Spot
+    //dotnet add package Microsoft.Extensions.Logging
+    //dotnet add package Microsoft.Extensions.Logging.Console
+    // installn Newtonsoft.Json
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba C2C Binance");
+
+            calc(args);
+
+        }
+
+        public static void calc(string[] args)
+        {
+            var data = Binance(args);
+
+            //Environment.Exit(0);
+
+            List<C2CHistory> C2Ch = new List<C2CHistory>();
+            
+            for (int i = 0; i < data.Result.Count; i++)
+            {
+
+                double nuorder = System.Convert.ToDouble(data.Result[i].unitPrice);
+                C2Ch.Add(new C2CHistory(data.Result[i].orderNumber.ToString(), data.Result[i].tradeType.ToString(), data.Result[i].asset.ToString(), data.Result[i].fiat.ToString(), data.Result[i].fiatSymbol.ToString(), System.Convert.ToDouble(data.Result[i].amount), System.Convert.ToDouble(data.Result[i].totalPrice), System.Convert.ToDouble(data.Result[i].unitPrice), data.Result[i].orderStatus.ToString(), System.Convert.ToDouble(data.Result[i].commission), data.Result[i].counterPartNickName.ToString(), data.Result[i].advertisementRole.ToString()));
+
+            }
+
+            var C2CBuy = C2Ch.Where(p => p.orderStatus == "COMPLETED" && p.TradeType == "BUY" && p.fiat == "DOP")
+            .OrderBy(p => p.orderStatus)
+            .ThenBy(p => p.TradeType);
+
+            var TotalCompras = C2CBuy.Sum(e => e.totalPrice);
+            var CantidadCompras = C2CBuy.Sum(e => e.amount);
+            var PriceCompra = C2CBuy.Sum(e => e.unitPrice);
+            var FeesBankCompras = C2CBuy.Sum(e => (e.amount * 0.0015));
+
+            Console.WriteLine("total monto comprado: " + Math.Round(TotalCompras, 2) + ", Cantidad Compras: " + Math.Round(CantidadCompras, 2) + ", FeesBank: " + Math.Round(FeesBankCompras, 2) + ", Price: " + Math.Round(PriceCompra, 2));
+
+            foreach (var C2C in C2CBuy)
+            {
+                Console.WriteLine("Order #: {0},{1},{2},{3},{4},{5},{6}", C2C.OrderNumber, C2C.TradeType, C2C.Asset, C2C.Amount, C2C.TotalPrice, C2C.UnitPrice, C2C.orderStatus);
+            }
+
+        }
+
+        public static async Task<dynamic> Binance(string[] args)
+        {
+
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            });
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+
+            HttpMessageHandler loggingHandler = new BinanceLoggingHandler(logger: logger);
+            //HttpClient httpClient = new HttpClient(handler: loggingHandler);
+            HttpClient httpClient = new HttpClient();
+
+            string apiKey = "FXkpHdw9HldvNtFmPe6iqsdTByhvWUU8UuHxCvzKuDhDewaMIejNbeVITsQptPVT";
+            string apiSecret = "25egnaFePsYeb6AbXyAf3MiHmdGGHZdxU7h0WpMOPAkyuBLWdKHGephXz6KKWPwg";
+
+            var c2c = new C2C(httpClient, apiKey: apiKey, apiSecret: apiSecret);
+
+
+
+            var strBuy = await c2c.GetC2cTradeHistory(Side.BUY);
+            var strSell = await c2c.GetC2cTradeHistory(Side.SELL);
+
+            dynamic rawBuy = JsonConvert.DeserializeObject<dynamic>(strBuy);
+            dynamic rawSell = JsonConvert.DeserializeObject<dynamic>(strSell);
+            dynamic jsBuy = rawBuy.data;
+            dynamic jsSell = rawSell.data;
+
+            jsBuy.Merge(jsSell, new JsonMergeSettings
+            {
+                // union array values together to avoid duplicates
+                MergeArrayHandling = MergeArrayHandling.Union
+            });
+
+            dynamic c2cresult = jsBuy;
+
+            return c2cresult;
+        }
+
+
+
+
+
+
+
+    }
+}
